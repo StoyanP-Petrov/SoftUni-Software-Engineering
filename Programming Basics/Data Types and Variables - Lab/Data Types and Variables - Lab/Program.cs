50a93835236717af275c5c360844d854a814f9cf
StoyanP-Petrov <stoianpzzzzzz@gmail.com>
2022-06-12 13:33:24 +0300

    First task- Convert Meters to Kilometers

---
+++ Programming Basics/Data Types and Variables - Lab/Data Types and Variables - Lab/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConvertMetersToKilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int metars = int.Parse(Console.ReadLine());
+            decimal kilometers = (decimal)metars / 1000;
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
