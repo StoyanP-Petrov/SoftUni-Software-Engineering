340322f3eae59f3648195a7fa2605d94ffd76ffd
StoyanP-Petrov <107347047+StoyanP-Petrov@users.noreply.github.com>
2022-07-08 19:33:37 +0300

    Add files via upload

---
+++ Programming Basics/Data Types and Variables - Lab/Centuries to Minutes/Concat Names/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Concat_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
