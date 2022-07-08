5984b6eddb7ed16d6a43e809140a683e806a8a7d
StoyanP-Petrov <107347047+StoyanP-Petrov@users.noreply.github.com>
2022-07-08 19:36:47 +0300

    Add files via upload

---
+++ Programming Basics/Data Types and Variables - Lab/Reversed Chars/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reversed_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
