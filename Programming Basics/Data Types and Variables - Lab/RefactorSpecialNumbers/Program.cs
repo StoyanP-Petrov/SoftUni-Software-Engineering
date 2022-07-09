c746cf60a0e2be1d0ee3f26690b50bd44e1077fc
StoyanP-Petrov <107347047+StoyanP-Petrov@users.noreply.github.com>
2022-07-09 20:30:53 +0300

    Add files via upload

---
+++ Programming Basics/Data Types and Variables - Lab/RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RefactorSpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
