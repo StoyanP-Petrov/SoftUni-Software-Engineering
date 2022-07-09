b5ad698ab0d2399a33528a66ec7146e54c9b6b5c
StoyanP-Petrov <107347047+StoyanP-Petrov@users.noreply.github.com>
2022-07-10 01:14:33 +0300

    Add files via upload

---
+++ Programming Basics/Data Types and Variables - Lab/Special Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                int currentNum = i;
+                int Sum = 0;
+
+                while (currentNum != 0)
+                {
+                    Sum += currentNum % 10;
+                    currentNum = currentNum / 10;
+                }
+
+                bool special = Sum == 5 || Sum == 7 || Sum == 11;
+
+                Console.WriteLine($"{i} -> {special}");
+            }
+        }
+    }
+}
