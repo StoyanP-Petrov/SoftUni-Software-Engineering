7bd5161e3631654f9a721850b1217271647273cb
StoyanP-Petrov <107347047+StoyanP-Petrov@users.noreply.github.com>
2022-07-09 20:30:28 +0300

    Add files via upload

---
+++ Programming Basics/Data Types and Variables - Lab/Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RefactorVolumeOfPyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
