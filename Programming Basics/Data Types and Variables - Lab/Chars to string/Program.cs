55658d809f1e1f6f3ebcbcd98186126e19d98378
StoyanP-Petrov <107347047+StoyanP-Petrov@users.noreply.github.com>
2022-07-10 01:15:43 +0300

    Add files via upload

---
+++ Programming Basics/Data Types and Variables - Lab/Chars to string/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CharsToString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var first = Console.ReadLine();
+            var second = Console.ReadLine();
+            var third = Console.ReadLine();
+
+            Console.WriteLine($"{first}{second}{third}");
+        }
+    }
+}
