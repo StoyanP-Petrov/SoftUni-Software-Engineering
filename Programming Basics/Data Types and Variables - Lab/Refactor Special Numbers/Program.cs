043c09806c5729aeb72a70515fdbf225029de4b6
StoyanP-Petrov <stoianpzzzzzz@gmail.com>
2022-07-13 10:20:06 +0300

    new solution

---
+++ Programming Basics/Data Types and Variables - Lab/Refactor Special Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RefactorSpecialNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lastNumber = int.Parse(Console.ReadLine());
+            for (int number = 1; number <= lastNumber; number++)
+            {
+                int currentNum = number;
+                int sum = 0;
+
+                while (currentNum > 0)
+                {
+                    sum += currentNum % 10;
+                    currentNum = currentNum / 10;
+                }
+
+                bool isSpecial = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", number, isSpecial);
+            }
+        }
+    }
+}
