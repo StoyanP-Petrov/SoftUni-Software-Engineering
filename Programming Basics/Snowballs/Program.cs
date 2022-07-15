2f5becea4cf281eb605eec74ccb648382453021d
StoyanP-Petrov <107347047+StoyanP-Petrov@users.noreply.github.com>
2022-07-16 00:44:31 +0300

    Add files via upload

---
+++ Programming Basics/Snowballs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+
+namespace Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            int bestSnow = 0;
+            int bestTime = 0;
+            int bestQuality = 0;
+            BigInteger bestValue = BigInteger.Zero;
+
+            for (int i = 0; i < count; i++)
+            {
+                int snow = int.Parse(Console.ReadLine());
+                int time = int.Parse(Console.ReadLine());
+                int quality = int.Parse(Console.ReadLine());
+
+                int divide = snow / time;
+                BigInteger value = BigInteger.Pow(divide, quality);
+
+                if (value >= bestValue)
+                {
+                    bestSnow = snow;
+                    bestTime = time;
+                    bestQuality = quality;
+                    bestValue = value;
+                }
+            }
+            Console.WriteLine($"{bestSnow} : {bestTime} = {bestValue} ({bestQuality})");
+        }
+    }
+}
