b11545edea19002d309772f83be2ef5e907589cb
StoyanP-Petrov <stoianpzzzzzz@gmail.com>
2022-07-22 21:46:24 +0300

    add new exercise

---
+++ Programming Basics/First Steps In Coding - Lab/InchesToCentimeters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InchesToCentimeters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double inch = a * 2.54;
+            Console.WriteLine(inch);
+        }
+    }
+}
